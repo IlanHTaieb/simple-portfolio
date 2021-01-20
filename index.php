1d253a0812b1755c6876dad76b2429fcba457f61
Ilan Taieb <ilan.taieb@outlook.fr>
2021-01-20 02:54:55 +0100

    first commit

--- index.php
+++ index.php
@@ -1,12 +1,78 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-  <?php require('layouts._meta') ?>
-  <?php require('layouts._links') ?>
-  <title>Ilan Taieb</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/css/app.css">
+    <title>Ilan Taieb</title>
 </head>
 <body>
-<?= $content; ?>
-<script src="{{ asset('js/app.js') }}"></script>
+<main id="main">
+    <section id="section">
+        <div id="menu">
+            <div id="menu_name" class="menu__item cybr-btn">
+                <h2 class="text-white orbitron">
+                    <div class="cybr-text">
+                        Ilan<span aria-hidden>_</span>
+                    </div>
+
+                    <span aria-hidden class="cybr-btn__punk">Ilan_</span>
+                    <span aria-hidden class="cybr-btn__tag">DEV</span>
+                </h2>
+            </div>
+
+            <div id="menu_skills" class="menu__item cybr-btn">
+                <h2 class="text-white orbitron">
+                    <div class="cybr-text">
+                        Skills<span aria-hidden>_</span>
+                    </div>
+
+                    <span aria-hidden class="cybr-btn__punk">Skills_</span>
+                    <span aria-hidden class="cybr-btn__tag">POW</span>
+                </h2>
+            </div>
+
+            <div id="menu_contact" class="menu__item cybr-btn">
+                <h2 class="text-white orbitron">
+                    <div class="cybr-text">
+                        Contact<span aria-hidden>_</span>
+                    </div>
+
+                    <span aria-hidden class="cybr-btn__punk">Contact_</span>
+                    <span aria-hidden class="cybr-btn__tag">CT</span>
+                </h2>
+            </div>
+        </div>
+
+        <div id="skills">
+            <ul class="list-group">
+                <li id="skills__laravel" class="list-group-item cybr-text skills__list">
+                    <p class="skills__text">Laravel</p> <span class="skills__score"></span>
+                </li>
+
+                <li id="skills__jquery" class="list-group-item cybr-text skills__list">
+                    <p class="skills__text">JQuery</p> <span class="skills__score"></span>
+                </li>
+
+                <li id="skills__php" class="list-group-item cybr-text skills__list">
+                    <p class="skills__text">PHP</p> <span class="skills__score"></span>
+                </li>
+
+                <li id="skills__mysql" class="list-group-item cybr-text skills__list">
+                    <p class="skills__text">MySQL</p> <span class="skills__score"></span>
+                </li>
+
+                <li id="skills__javascript" class="list-group-item cybr-text skills__list">
+                    <p class="skills__text">Javascript</p> <span class="skills__score"></span>
+                </li>
+
+                <li id="skills__bootstrap" class="list-group-item cybr-text skills__list">
+                    <p class="skills__text">Bootstrap</p> <span class="skills__score"></span>
+                </li>
+            </ul>
+        </div>
+    </section>
+</main>
+<script src="/public/js/app.js"></script>
 </body>
 </html>
